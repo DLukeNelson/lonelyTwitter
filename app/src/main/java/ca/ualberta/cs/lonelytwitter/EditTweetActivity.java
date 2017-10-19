e8ca4b01fabbfa115a5dbd254cc5976de715c264
D. Luke Nelson <dlnelson@ualberta.ca>
2017-10-18 18:50:38 -0600

    Solution to lab7.

---
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/EditTweetActivity.java
@@ -0,0 +1,22 @@
+package ca.ualberta.cs.lonelytwitter;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class EditTweetActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_tweet);
+        TextView tweet = (TextView) findViewById(R.id.answerGoesHere);
+
+        Intent intent = getIntent();
+        String s = intent.getStringExtra("Tweet");
+        tweet.setText(s);
+
+    }
+}
